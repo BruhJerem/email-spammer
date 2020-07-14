393934dcc6f1e59e24e7b7c4e441200b6572311a
RemCoding <jeremie.bruhwiler@epitech.eu>
2020-07-14 22:06:17 +0200

    Refacto code

--- spammer.py
+++ spammer.py
@@ -22,40 +22,30 @@ def main():
                              cycle_cursor=True,
                              clear_screen=True)
 
-    edit_menu_title = "  Authentication\n"
-    edit_menu_items = ["Edit Config", "Save Settings", "Back to Main Menu"]
-    edit_menu = TerminalMenu(edit_menu_items,
-                             edit_menu_title,
-                             main_menu_cursor,
-                             main_menu_cursor_style,
-                             main_menu_style,
-                             cycle_cursor=True,
-                             clear_screen=True)
-    Spammer(main_menu, edit_menu)
+    Spammer(main_menu)
 
 class Spammer():
-    def __init__(self, main_menu, edit_menu):
-        main_menu_exit = False
-        # edit_menu_back = False
-        while not main_menu_exit:
-            main_sel = main_menu.show()
+    def __init__(self, main_menu):
+        menu_exit = False
+        while not menu_exit:
+            selection = main_menu.show()
 
-            if main_sel == 0:
+            if selection == 0:
                 self.smtp_server = 'smtp.gmail.com'
                 self.port = 587
                 self.set_server = "gmail"
                 self.auth()
-                main_menu_exit = True
+                menu_exit = True
                 self.exit()
-            elif main_sel == 1:
+            elif selection == 1:
                 self.smtp_server = 'smtp.mail.yahoo.com'
                 self.port = 25
                 self.set_server = "yahoo"
                 self.auth()
-                main_menu_exit = True
+                menu_exit = True
                 self.exit()
-            elif main_sel == 2:
-                main_menu_exit = True
+            elif selection == 2:
+                menu_exit = True
                 self.exit()
       
     def exit(self):
